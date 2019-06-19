7d2cd8b9c9ff4d865b70f8a59d291c0085ddf8eb
Mohamed Hossam <moh98hos@gmail.com>
2019-06-19 21:39:11 +0200

    Edit Business card Activity

---
+++ app/src/main/java/com/graduation/ewallet/Main/HomeFragment/EditBusinessCardActivity.java
@@ -0,0 +1,15 @@
+package com.graduation.ewallet.Main.HomeFragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.graduation.ewallet.R;
+
+public class EditBusinessCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_business_card_activity_);
+    }
+}
